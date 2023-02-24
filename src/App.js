83f964dfc03cd14f1b7d9b7c15b956592eec50f1
KarlaMarija <kmarun00@fesb.hr>
2023-02-24 13:12:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [term, setTerm] = useState('everything');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+    try {
+      
+        const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=oBbosWF3D5w2uUk8s6M8sTNLGvC2OEDa`);
+        const articles = await res.json();
+        console.log(articles);
+        setArticles(articles.response.docs);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+    fetchArticles();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        {articles.map((article) => {
+          const {abstract, headline: { main }, byline: { original }, lead_paragraph, news_desk, section_name, web_url, _id, word_count} = article;
+
+          return (
+            <article key={_id}>
+              <h2>{main}</h2>
+              <p>{abstract}</p>
+              <a href={web_url} target="_blank">Web Resource</a>
+              <p>{lead_paragraph}</p>
+
+              <ul>
+                <li>{original}</li>
+                <li>{news_desk}</li>
+                <li>{section_name}</li>
+                <li>{word_count}</li>
+              </ul>
+            </article>
+          )
+        })}
+      </section>
+    </>
   );
 }
 
